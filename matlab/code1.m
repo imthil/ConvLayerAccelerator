3aeaa35681b50e9960485cc4863b14aa1e607432
Thil <thilina.arachchi@gmail.com>
2017-03-16 16:21:08 -0700

    "Updated variable names "

--- matlab/code1.m
+++ matlab/code1.m
@@ -2,28 +2,38 @@
 % Email address: thilina@ieee.org  
 % March 2017; Last revision: 7-March-2017
 
+% Layer Params
+W = 224;    % Dim of Input Map
+M = 48;     % No. Output Features
+N = 3;      % No. Input Features
+K = 11;     % Filter Block Size
+S = 4;      % Stride
+P = 0;      % Padding
+R = round((W-K+2*P)/S + 1); % Dim of Ouput Map
+C = R;
+
 % Normalize the image, resize, convert to single precision and display
 a = imnorm('lena512color.tiff');
-a = imresize(a, [224 224]);
+a = imresize(a, [W W]);
 x = im2single(a);
 figure(1); clf; imagesc(x);
 
 % Implement a 11x11x3 48 linear filter bank with random coefficients
-w = randn(11, 11, 3, 48, 'single');
+w = randn(K, K, N, M, 'single');
 
 % Start profiling
 profile on;
 
 % Perform convolution of the input as per Code 1
-y=zeros(54,54,48);
-for row = 1:54
-    for col = 1:54
-        for to = 1:48
-            for ti = 1:3
-                for i = 1:11
-                    for j = 1:11
+y=zeros(R, C, M);
+for row = 1:R 
+    for col = 1:C
+        for to = 1:M
+            for ti = 1:N
+                for i = 1:K
+                    for j = 1:K
                         y(row,col,to) = y(row,col,to)... 
-                            + w(i,j,ti,to) * x(4*(row-1)+i,4*(col-1)+j,ti); 
+                            + w(i,j,ti,to) * x(S*(row-1)+i,S*(col-1)+j,ti); 
                     end
                 end
             end
