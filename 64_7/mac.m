b8130c6ae1e188de039e77d1ffee7e1f37002a1b
Thil <thilina.arachchi@gmail.com>
2017-03-17 17:30:13 -0700

    "Added <64,7> matlab"

---
+++ 64_7/mac.m
@@ -0,0 +1,5 @@
+function Tm = mac( M, N, W, mm, nn )
+%MAC simply multiplies and accumalates
+Tm = M(mm) + N(nn)*W(nn);
+end
+
---
+++ 64_7/sixfourseven.m
@@ -0,0 +1,33 @@
+% Author: Thilina Mallawa Arachchi
+% Email address: thilina@ieee.org  
+% March 2017; Last revision: 17-March-2017
+
+%Params
+Tm = 64;
+Tn = 7;
+
+% Set input and output feature map
+N = single(ones(1,Tn));
+M = single(zeros(1,Tm));
+
+% Set filter weights
+% For simplicity lets assume all wieghts are the same
+W = single([1 0.95 0.85 0.25 0 0 0.45]); 
+
+for mm = 1:Tm
+    for nn = 1:Tn
+        % Processing Element
+        M(mm) = mac( M, N, W, mm, nn );
+    end
+end
+
+checker = 1;
+for check = 1:Tm-1
+    if M(check) == M(check+1)
+        checker = checker + 1;
+    end
+end
+
+if checker == Tm 
+    fprintf('Passed with value %d for each %d feature(s)\n', M(1), Tm);
+end
